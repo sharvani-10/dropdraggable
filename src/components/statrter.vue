d644d151d22d026222c7018aece41de5d15470e6
Vanaparthi Sharvani <sharvani.vanaparthi@stayconnect.com>
2025-04-03 17:55:35 +0530

    second commit

---
+++ src/components/statrter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    import axios from 'axios';
+    const sampleData=ref([])
+    const fetchData =async()=>{
+        try {
+            const res = await axios.get("http://192.168.11.71:8000/todos/");
+            if(res.status===200){
+                sampleData.value=res.data;
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    onMounted(()=>{
+        fetchData()
+    })
+</script>
+
+<template>
+    <v-card>
+        {{ sampleData }}
+    </v-card>
+</template>
